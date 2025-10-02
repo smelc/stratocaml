763eaccb7633d326b9f0683d2a0e09498f255358
Clément Hurlin <clement.hurlin@tweag.io>
2025-10-02 11:52:41 +0200

    CI: enable dune cache + check formatting of ml sources and opam files

--- test/test_terraform_state.ml
+++ test/test_terraform_state.ml
@@ -1,14 +1,18 @@
 open Stratocaml
 
 let test_deserialize_state () =
-  let json_string = In_channel.with_open_text "example.tfstate" In_channel.input_all in
+  let json_string =
+    In_channel.with_open_text "example.tfstate" In_channel.input_all
+  in
   let yojson = Yojson.Safe.from_string json_string in
   let state = Terraform_state.state_v4_of_yojson yojson in
   Alcotest.(check int) "version" 4 state.version;
   Alcotest.(check string) "terraform_version" "1.0.0" state.terraform_version
 
 let test_deserialize_complex_state () =
-  let json_string = In_channel.with_open_text "complex.tfstate" In_channel.input_all in
+  let json_string =
+    In_channel.with_open_text "complex.tfstate" In_channel.input_all
+  in
   let yojson = Yojson.Safe.from_string json_string in
   let state = Terraform_state.state_v4_of_yojson yojson in
   Alcotest.(check string) "terraform_version" "1.2.3" state.terraform_version;
@@ -16,10 +20,12 @@ let test_deserialize_complex_state () =
 
 let () =
   let open Alcotest in
-  run
-    "Terraform State"
-    [ ( "deserialization"
-      , [ test_case "Deserialize example state" `Quick test_deserialize_state
-        ; test_case "Deserialize complex state" `Quick test_deserialize_complex_state
-        ] )
+  run "Terraform State"
+    [
+      ( "deserialization",
+        [
+          test_case "Deserialize example state" `Quick test_deserialize_state;
+          test_case "Deserialize complex state" `Quick
+            test_deserialize_complex_state;
+        ] );
     ]
